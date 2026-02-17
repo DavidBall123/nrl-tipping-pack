948afe3e7915a5736f6cd4cf99eba9b1f04f99a8
David <david_ball01@hotmail.com>
2026-02-17 18:34:29 +0000

    fix: configure ESLint for TypeScript sources

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,26 @@
 import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
 
 export default [
+  {
+    ignores: ["dist/**", "node_modules/**"]
+  },
   js.configs.recommended,
   {
     files: ["src/**/*.ts"],
     languageOptions: {
+      parser: tsParser,
       ecmaVersion: "latest",
-      sourceType: "module"
+      sourceType: "module",
+      globals: globals.node
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin
     },
     rules: {
+      ...tsPlugin.configs.recommended.rules,
       "no-console": "off"
     }
   }
